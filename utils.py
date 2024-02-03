10600fbbd25c4c9c418071c1aca9afca439d3a28
Luieitalian <enderbud125@hotmail.com>
2024-02-03 18:48:26 +0300

    Revert "rd"
    This reverts commit 4a6b08aed942665e507f90f41555d49a0926f967.

--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 from selenium.webdriver.common.by import By
 
 def model_product(product):
+    def model_productt():
+        pass
+
+    model_productt()
     product_name = product.find_element(By.CSS_SELECTOR, "span.gXitDI").text
     product_img = product.find_element(By.TAG_NAME, "img").get_attribute("src")
     product_price = product.find_element(By.CSS_SELECTOR, "span.grTSzD").text
