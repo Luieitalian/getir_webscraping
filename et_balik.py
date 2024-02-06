94e5ca3068d6c66bba33765d08acd89a8a5539fc
Luieitalian <enderbud125@hotmail.com>
2024-02-06 23:21:38 +0300

    chore: organise code

--- et_balik.py
+++ et_balik.py
@@ -4,11 +4,11 @@ from utils import save_images
 from selenium import webdriver
 
 category = "et_balik"
-et_balik_url = "https://getir.com/buyuk/kategori/et-tavuk-balik-P1593VdPBd/"
+url = "https://getir.com/buyuk/kategori/et-tavuk-balik-P1593VdPBd/"
 identifiers = {"article": "ccXlDA",
                "name": "jFvYQy", "img": "hDQjIU", "price": "jmELbE", "amount": "dmgfcc"}
 
 driver = webdriver.Firefox()
-write_to_json(category, get_products(et_balik_url, identifiers, driver))
+write_to_json(category, get_products(url, identifiers, driver))
 save_images(category)
 driver.quit()
